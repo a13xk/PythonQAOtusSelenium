b0f88f383fc0ba3b916726aed8b5ad58a5a281db
Alexander Knyazev <26230870+a13xk@users.noreply.github.com>
2020-05-13 10:17:26 +0300

    Add explicit waits for main page tests

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,7 +1,8 @@
-from locators import MainPage
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+from locators import MainPage
 
 
 class TestMainPage:
