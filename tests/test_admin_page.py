b3d134304065cf8172a4ef449d195f69a1fca780
Alexander Knyazev <26230870+a13xk@users.noreply.github.com>
2020-05-13 11:31:22 +0300

    Add login/logout test for admin login page

--- tests/test_admin_page.py
+++ tests/test_admin_page.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+from configuration import OpenCartConfiguration
 from locators.admin_page import AdminPage
 
 
@@ -31,4 +32,23 @@ class TestAdminPage:
         wait = WebDriverWait(driver=browser, timeout=5)
         wait.until(EC.visibility_of_element_located(locator=(By.LINK_TEXT, AdminPage.LINK_TEXT_OPENCART)))
     #
+
+    def test_login_logout(self, browser):
+        wait = WebDriverWait(driver=browser, timeout=15)
+        input_username = browser.find_element_by_id(id_=AdminPage.ID_INPUT_USERNAME)
+        input_username.click()
+        input_username.send_keys(OpenCartConfiguration.ADMIN_USERNAME)
+
+        input_password = browser.find_element_by_id(id_=AdminPage.ID_INPUT_PASSWORD)
+        input_password.click()
+        input_password.send_keys(OpenCartConfiguration.ADMIN_PASSWORD)
+
+        login_button = browser.find_element_by_xpath(xpath=AdminPage.XPATH_LOGIN_BUTTON)
+        login_button.click()
+        wait.until(EC.visibility_of_element_located(locator=(By.XPATH, AdminPage.XPATH_DASHBOARD_HEADING)))
+
+        logout_button = browser.find_element_by_xpath(xpath=AdminPage.XPATH_LOGOUT_BUTTON)
+        logout_button.click()
+
+        wait.until(EC.visibility_of_element_located(locator=(By.ID, AdminPage.ID_INPUT_PASSWORD)))
 #
