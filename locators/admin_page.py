d4f642617feaceb497c25ea8101f9044e9127d8f
Alexander Knyazev <26230870+a13xk@users.noreply.github.com>
2020-05-13 11:30:23 +0300

    Add locators for admin login page

--- locators/admin_page.py
+++ locators/admin_page.py
@@ -1,7 +1,12 @@
 class AdminPage:
     ID_HEADER_LOGO = "header-logo"
+    ID_INPUT_USERNAME = "input-username"
+    ID_INPUT_PASSWORD = "input-password"
     CSS_USERNAME = "label[for='input-username']"
     XPATH_FORGOTTEN_PASSWORD = "//div[@class='form-group']/span/a"
+    XPATH_LOGIN_BUTTON = "//button[@type='submit' and contains(text(), 'Login')]"
+    XPATH_DASHBOARD_HEADING = "//div[@id='content']//h1[contains(text(), 'Dashboard')]"
+    XPATH_LOGOUT_BUTTON = "//a[contains(@href, 'logout')]"
     CLASS_PANEL_HEADING = "panel-heading"
     LINK_TEXT_OPENCART = "OpenCart"
 #
