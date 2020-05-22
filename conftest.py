a5de81d8f76b5aa760ad7a3b894d8a23189e7229
Alexander Knyazev <26230870+a13xk@users.noreply.github.com>
2020-05-22 11:14:57 +0300

    Convert admin login page tests to Page Object Model paradigm

--- conftest.py
+++ conftest.py
@@ -4,6 +4,7 @@ from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.opera.options import Options as OperaOptions
 
 pytest_plugins = [
+    "fixtures.fixtures_admin_login_page",
     "fixtures.fixtures_administration_page",
     "fixtures.fixtures_catalog_page",
     "fixtures.fixtures_login_page",
---
+++ tests/test_admin_login_page.py
@@ -0,0 +1,36 @@
+from configuration import OpenCartConfiguration
+from pages.administration_page import AdministrationPage
+
+
+class TestAdminLoginPage:
+
+    def test_header_logo(self, admin_login_page):
+        assert admin_login_page.find_element(locator=admin_login_page.IMG_LOGO)
+    #
+
+    def test_username_label(self, admin_login_page):
+        assert admin_login_page.find_element(locator=admin_login_page.LABEL_FOR_USERNAME)
+    #
+
+    def test_forgotten_password_link(self, admin_login_page):
+        assert admin_login_page.find_element(locator=admin_login_page.A_FORGOTTEN_PASSWORD)
+    #
+
+    def test_panel_heading(self, admin_login_page):
+        assert admin_login_page.find_element(locator=admin_login_page.DIV_PANEL_HEADING)
+    #
+
+    def test_opencart_link(self, admin_login_page):
+        assert admin_login_page.find_element(locator=admin_login_page.A_OPENCART)
+    #
+
+    def test_login_logout(self, admin_login_page, browser):
+        admin_login_page.log_in(
+            username=OpenCartConfiguration.ADMIN_USERNAME,
+            password=OpenCartConfiguration.ADMIN_PASSWORD
+        )
+        administration_page = AdministrationPage(driver=browser)
+        assert administration_page.find_element(locator=administration_page.DIV_DASHBOARD)
+        administration_page.log_out()
+    #
+#
