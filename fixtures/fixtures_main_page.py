41a01cc9733a36bad91a8fdf333fc26be7cefc86
Alexander Knyazev <26230870+a13xk@users.noreply.github.com>
2020-06-07 20:21:57 +0300

    Add tests for main page using 'remote browser' fixture

--- fixtures/fixtures_main_page.py
+++ fixtures/fixtures_main_page.py
@@ -10,3 +10,11 @@ def main_page(browser: webdriver, base_class_logging: bool) -> MainPage:
     main_page.open_page()
     return main_page
 #
+
+
+@pytest.fixture(scope="function")
+def remote_main_page(remote_browser: webdriver, base_class_logging: bool) -> MainPage:
+    main_page = MainPage(driver=remote_browser, logging_enabled=base_class_logging)
+    main_page.open_page()
+    return main_page
+#
---
+++ tests/test_remote_main_page.py
@@ -0,0 +1,22 @@
+class TestRemoteMainPage:
+
+    def test_remote_slideshow_by_id(self, remote_main_page):
+        assert remote_main_page.find_element(locator=remote_main_page.DIV_ID_SLIDESHOW)
+    #
+
+    def test_slideshow_by_css(self, remote_main_page):
+        assert remote_main_page.find_element(locator=remote_main_page.DIV_CSS_SLIDESHOW)
+    #
+
+    def test_slideshow_by_xpath(self, remote_main_page):
+        assert remote_main_page.find_element(locator=remote_main_page.DIV_XPATH_SLIDESHOW)
+    #
+
+    def test_slideshow_by_class_name(self, remote_main_page):
+        assert remote_main_page.find_element(locator=remote_main_page.DIV_CLASS_SLIDESHOW)
+    #
+
+    def test_slideshow_by_link_text(self, remote_main_page):
+        assert remote_main_page.find_element(locator=remote_main_page.A_YOUR_STORE)
+    #
+#
