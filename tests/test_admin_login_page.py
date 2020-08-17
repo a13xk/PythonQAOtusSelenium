1895ad49964765dc55337a1d661774d33a94f443
Alexander Knyazev <26230870+a13xk@users.noreply.github.com>
2020-08-17 17:03:49 +0300

    Add allure.title decorators

--- tests/test_admin_login_page.py
+++ tests/test_admin_login_page.py
@@ -1,29 +1,37 @@
+import allure
+
 from configuration import OpenCart
 from pages.administration_page import AdministrationPage
 
 
 class TestAdminLoginPage:
 
+    @allure.title("Check logo image")
     def test_header_logo(self, admin_login_page):
         assert admin_login_page.find_element(locator=admin_login_page.IMG_LOGO)
     #
 
+    @allure.title("Check username input label")
     def test_username_label(self, admin_login_page):
         assert admin_login_page.find_element(locator=admin_login_page.LABEL_FOR_USERNAME)
     #
 
+    @allure.title("Check restore forgotten password link by XPath")
     def test_forgotten_password_link(self, admin_login_page):
         assert admin_login_page.find_element(locator=admin_login_page.A_FORGOTTEN_PASSWORD)
     #
 
+    @allure.title('Check "Please enter your login details." heading')
     def test_panel_heading(self, admin_login_page):
         assert admin_login_page.find_element(locator=admin_login_page.DIV_PANEL_HEADING)
     #
 
+    @allure.title("Check OpenCart hyperlink")
     def test_opencart_link(self, admin_login_page):
         assert admin_login_page.find_element(locator=admin_login_page.A_OPENCART)
     #
 
+    @allure.title("Log in to administration page and then log out")
     def test_login_logout(self, admin_login_page, browser, base_class_logging):
         admin_login_page.log_in(
             username=OpenCart.ADMIN_USERNAME,
